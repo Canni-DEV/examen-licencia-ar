b5d4168289f4ff7d015eb33838e8732ab364f191
Canni-DEV <88213356+Canni-DEV@users.noreply.github.com>
2025-08-19 02:51:58 -0300

    feat: animate page transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import AppRouter from './router'
 import { Layout } from './ui'
 
-/** App envuelve el router para animaciones globales */
+/**
+ * App envuelve el router y aplica animaciones entre páginas.
+ * Cada cambio de ruta se anima usando framer-motion.
+ */
+function AnimatedRoutes() {
+    const location = useLocation()
+    return (
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={location.pathname}
+                initial={{ opacity: 0, y: 16 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -16 }}
+                transition={{ duration: 0.2 }}
+            >
+                <AppRouter />
+            </motion.div>
+        </AnimatePresence>
+    )
+}
+
 export default function App() {
-    // Nota: animamos a nivel de páginas individuales para simplicidad, ver pages/*
     return (
         <HashRouter>
             <Layout>
-                {/* AppRouter renderiza las páginas */}
-                <AppRouter />
+                {/* AppRouter renderiza las páginas con animación */}
+                <AnimatedRoutes />
             </Layout>
         </HashRouter>
     )
